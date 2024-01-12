c7500dfd88754d939586da0a0218a87000b3e1c3
sh0rtmiami <149796742+sh0rtmiami@users.noreply.github.com>
2024-01-12 08:45:31 +0000

    Update contact.ts

--- config/contact.ts
+++ config/contact.ts
@@ -17,16 +17,16 @@ export interface Contact {
 
 export const contact: Contact = {
   twitter: '@karan_6864',
-  site: 'karanpratapsingh.com',
-  calendly: 'https://calendly.com/karanpratapsingh',
+  site: 'joshwagenbach.com',
+  calendly: 'https://calendly.com/maltewagenbach',
   links: {
     github: 'https://github.com/karanpratapsingh',
-    linkedin: 'https://linkedin.com/in/karan99',
+    linkedin: 'https://linkedin.com/in/maltewagenbach',
     googlescholar:
       'https://scholar.google.com/citations?user=8wIfeAsAAAAJ&hl=en',
     twitter: 'https://twitter.com/karan_6864',
     youtube: 'https://www.youtube.com/c/KaranPratapSingh',
-    email: 'mailto:contact@karanpratapsingh.com',
-    buymeacoffee: 'https://www.buymeacoffee.com/karanps',
+    email: 'mailto:malte@joshwagenbach.com',
+    buymeacoffee: 'https://www.buymeacoffee.com/maltewagenbach',
   },
 };
